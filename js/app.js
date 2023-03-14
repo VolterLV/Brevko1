ce7ec06d3756c325a1afc73fe4feb56f4a96806b
TelevizorFisher <>
2023-03-14 15:41:49 +0200

    v3

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@
             document.documentElement.classList.add(className);
         }));
     }
+    function getHash() {
+        if (location.hash) return location.hash.replace("#", "");
+    }
     let _slideUp = (target, duration = 500, showmore = 0) => {
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
@@ -208,6 +211,10 @@
                 document.documentElement.classList.toggle("menu-open");
             }
         }));
+    }
+    function menuClose() {
+        bodyUnlock();
+        document.documentElement.classList.remove("menu-open");
     }
     function functions_FLS(message) {
         setTimeout((() => {
@@ -503,6 +510,44 @@
         }
     }
     modules_flsModules.popup = new Popup({});
+    let gotoblock_gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
+        const targetBlockElement = document.querySelector(targetBlock);
+        if (targetBlockElement) {
+            let headerItem = "";
+            let headerItemHeight = 0;
+            if (noHeader) {
+                headerItem = "header.header";
+                const headerElement = document.querySelector(headerItem);
+                if (!headerElement.classList.contains("_header-scroll")) {
+                    headerElement.style.cssText = `transition-duration: 0s;`;
+                    headerElement.classList.add("_header-scroll");
+                    headerItemHeight = headerElement.offsetHeight;
+                    headerElement.classList.remove("_header-scroll");
+                    setTimeout((() => {
+                        headerElement.style.cssText = ``;
+                    }), 0);
+                } else headerItemHeight = headerElement.offsetHeight;
+            }
+            let options = {
+                speedAsDuration: true,
+                speed,
+                header: headerItem,
+                offset: offsetTop,
+                easing: "easeOutQuad"
+            };
+            document.documentElement.classList.contains("menu-open") ? menuClose() : null;
+            if ("undefined" !== typeof SmoothScroll) (new SmoothScroll).animateScroll(targetBlockElement, "", options); else {
+                let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
+                targetBlockElementPosition = headerItemHeight ? targetBlockElementPosition - headerItemHeight : targetBlockElementPosition;
+                targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
+                window.scrollTo({
+                    top: targetBlockElementPosition,
+                    behavior: "smooth"
+                });
+            }
+            functions_FLS(`[gotoBlock]: Юхуу...їдемо до ${targetBlock}`);
+        } else functions_FLS(`[gotoBlock]: Йой... Такого блоку немає на сторінці: ${targetBlock}`);
+    };
     function ssr_window_esm_isObject(obj) {
         return null !== obj && "object" === typeof obj && "constructor" in obj && obj.constructor === Object;
     }
@@ -3238,13 +3283,20 @@
                 observeParents: true,
                 slidesPerView: 2,
                 spaceBetween: 30,
+                lazy: true,
                 speed: 800,
+                loop: true,
+                parallax: true,
                 navigation: {
                     prevEl: ".swiper-button-prev",
                     nextEl: ".swiper-button-next"
                 },
                 breakpoints: {
-                    640: {
+                    320: {
+                        slidesPerView: 1.1,
+                        spaceBetween: 15
+                    },
+                    400: {
                         slidesPerView: 2,
                         spaceBetween: 10
                     },
@@ -3258,8 +3310,7 @@
                     },
                     1268: {
                         slidesPerView: 3,
-                        spaceBetween: 20,
-                        autoHeight: true
+                        spaceBetween: 20
                     }
                 },
                 on: {}
@@ -3270,14 +3321,20 @@
                 observeParents: true,
                 slidesPerView: 2,
                 spaceBetween: 30,
+                lazy: true,
                 speed: 800,
                 loop: true,
+                parallax: true,
                 navigation: {
                     prevEl: ".swiper-button-prev",
                     nextEl: ".swiper-button-next"
                 },
                 breakpoints: {
-                    640: {
+                    320: {
+                        slidesPerView: 1.1,
+                        spaceBetween: 15
+                    },
+                    400: {
                         slidesPerView: 2,
                         spaceBetween: 10
                     },
@@ -3291,8 +3348,7 @@
                     },
                     1268: {
                         slidesPerView: 3,
-                        spaceBetween: 20,
-                        autoHeight: true
+                        spaceBetween: 20
                     }
                 },
                 on: {}
@@ -3302,15 +3358,21 @@
                 observer: true,
                 observeParents: true,
                 slidesPerView: 2,
+                lazy: true,
                 spaceBetween: 30,
                 speed: 800,
                 loop: true,
+                parallax: true,
                 navigation: {
                     prevEl: ".swiper-button-prev",
                     nextEl: ".swiper-button-next"
                 },
                 breakpoints: {
-                    640: {
+                    320: {
+                        slidesPerView: 1.1,
+                        spaceBetween: 15
+                    },
+                    400: {
                         slidesPerView: 2,
                         spaceBetween: 10
                     },
@@ -3324,8 +3386,7 @@
                     },
                     1268: {
                         slidesPerView: 3,
-                        spaceBetween: 20,
-                        autoHeight: true
+                        spaceBetween: 30
                     }
                 },
                 on: {}
@@ -3335,9 +3396,11 @@
                 observer: true,
                 observeParents: true,
                 slidesPerView: 2,
+                lazy: true,
                 spaceBetween: 20,
                 autoHeight: true,
                 speed: 800,
+                parallax: true,
                 navigation: {
                     prevEl: ".swiper-button-prev",
                     nextEl: ".swiper-button-next"
@@ -3368,6 +3431,51 @@
         initSliders();
     }));
     let addWindowScrollEvent = false;
+    function pageNavigation() {
+        document.addEventListener("click", pageNavigationAction);
+        document.addEventListener("watcherCallback", pageNavigationAction);
+        function pageNavigationAction(e) {
+            if ("click" === e.type) {
+                const targetElement = e.target;
+                if (targetElement.closest("[data-goto]")) {
+                    const gotoLink = targetElement.closest("[data-goto]");
+                    const gotoLinkSelector = gotoLink.dataset.goto ? gotoLink.dataset.goto : "";
+                    const noHeader = gotoLink.hasAttribute("data-goto-header") ? true : false;
+                    const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 500;
+                    const offsetTop = gotoLink.dataset.gotoTop ? parseInt(gotoLink.dataset.gotoTop) : 0;
+                    if (modules_flsModules.fullpage) {
+                        const fullpageSection = document.querySelector(`${gotoLinkSelector}`).closest("[data-fp-section]");
+                        const fullpageSectionId = fullpageSection ? +fullpageSection.dataset.fpId : null;
+                        if (null !== fullpageSectionId) {
+                            modules_flsModules.fullpage.switchingSection(fullpageSectionId);
+                            document.documentElement.classList.contains("menu-open") ? menuClose() : null;
+                        }
+                    } else gotoblock_gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
+                    e.preventDefault();
+                }
+            } else if ("watcherCallback" === e.type && e.detail) {
+                const entry = e.detail.entry;
+                const targetElement = entry.target;
+                if ("navigator" === targetElement.dataset.watch) {
+                    document.querySelector(`[data-goto]._navigator-active`);
+                    let navigatorCurrentItem;
+                    if (targetElement.id && document.querySelector(`[data-goto="#${targetElement.id}"]`)) navigatorCurrentItem = document.querySelector(`[data-goto="#${targetElement.id}"]`); else if (targetElement.classList.length) for (let index = 0; index < targetElement.classList.length; index++) {
+                        const element = targetElement.classList[index];
+                        if (document.querySelector(`[data-goto=".${element}"]`)) {
+                            navigatorCurrentItem = document.querySelector(`[data-goto=".${element}"]`);
+                            break;
+                        }
+                    }
+                    if (entry.isIntersecting) navigatorCurrentItem ? navigatorCurrentItem.classList.add("_navigator-active") : null; else navigatorCurrentItem ? navigatorCurrentItem.classList.remove("_navigator-active") : null;
+                }
+            }
+        }
+        if (getHash()) {
+            let goToHash;
+            if (document.querySelector(`#${getHash()}`)) goToHash = `#${getHash()}`; else if (document.querySelector(`.${getHash()}`)) goToHash = `.${getHash()}`;
+            goToHash ? gotoblock_gotoBlock(goToHash, true, 500, 20) : null;
+        }
+    }
     setTimeout((() => {
         if (addWindowScrollEvent) {
             let windowScroll = new Event("windowScroll");
@@ -3408,4 +3516,5 @@
     isWebp();
     menuInit();
     spollers();
+    pageNavigation();
 })();
